3e6dc2247fd820e879ad833fc25a42d50aab8234
bodikqlar <qlarmore@gmail.com>
2015-05-21 16:43:14 +0300

    Add image fix for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -70,7 +70,7 @@ Rails.application.configure do
 
   # Send deprecation notices to registered listeners.
   # config.active_support.deprecation = :notify
-
+  config.assets.precompile = ['*.js', '*.css', '*.css.erb']
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
